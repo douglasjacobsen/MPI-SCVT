1713a1ebd55784e9fdc7138ba0e75aa69d8e48de
Douglas Jacobsen <jacobsen.douglas@gmail.com>
2014-08-14 12:34:16 -0600

    Adding eddy closure density function

--- scvt-mpi.cpp
+++ scvt-mpi.cpp
@@ -2908,7 +2908,7 @@ int writeRestartFileRetainNC( const int it, const vector<pnt> &points ) {/*{{{*/
 int writeRestartFileOverwriteTXT( const int it ) {/*{{{*/
 
 	char temp[32];
-	sprintf(temp,"point_restart.dat\0");
+	sprintf(temp,"point_restart.dat");
 
 	ofstream pts_out(temp);
 
@@ -2922,7 +2922,7 @@ int writeRestartFileOverwriteTXT( const int it ) {/*{{{*/
 int writeRestartFileRetainTXT( const int it ) {/*{{{*/
 	
 	char temp[32];
-	sprintf(temp,"point_restart_%d.dat\0",it);
+	sprintf(temp,"point_restart_%d.dat",it);
 
 	ofstream pts_out(temp);
 
@@ -2936,7 +2936,7 @@ int writeRestartFileRetainTXT( const int it ) {/*{{{*/
 
 double density(const pnt &p){/*{{{*/
 	//density returns the value of the density function at point p
-	return 1.0; // Uniform density
+	//return 1.0; // Uniform density
 
 	/* Density function for Shallow Water Test Case 5 
 	pnt cent;
@@ -3010,18 +3010,51 @@ double ellipse_density(const pnt &p, double lat_c, double lon_c, double lat_widt
 }/*}}}*/
 double pop_lowres_density(const pnt &p){/*{{{*/
     double dtr;
-    double density, gamma, lat, lat_cent, width;
+    double density, lat;
+
+	double cellWidthLL, cellWidthML, cellWidthHL;
+	double densityLL, densityML, densityHL;
+	double minCellWidth;
+	double latStepFunction, lat_centLL, lat_centHL;
+	double widthLL, widthHL;
+	double dLat, dLatRadians;
 
 	dtr = M_PI/180.0;
 
-    lat_cent = 25.0 * dtr;
-    width = 6.0 * dtr;
-    gamma = powf(1.0 / 1.95, 4.0);
-    lat = p.getLat();
+	//Here is the key to variable names:
+	//LL low latitude
+	//ML mid latitude
+	//HL high latitude
 
-    density = ((1.0-gamma) * (1.0 + tanh( (lat_cent - fabs(lat)) / width)) / 2.0) + gamma;
+	cellWidthLL = 30.0;
+	cellWidthML = 70.0;
+	cellWidthHL = 35.0;
+	minCellWidth = min(cellWidthLL, min(cellWidthML, cellWidthHL));
 
-    return density;
+	lat = p.getLat(); // Latitude of point to compute density for
+
+	dLat = 0.1;
+	dLatRadians = dLat*dtr;
+
+	latStepFunction = 40.0 * dtr; // Latitude to change from LL to HL function
+
+	lat_centLL = 15.0 * dtr; // In Radians - Position of center of transition region
+	lat_centHL = 73.0 * dtr; // In Radians - Position of center of transition region
+
+	widthLL = 6.0 * dtr; // In Radians - Width of transition region
+	widthHL = 9.0 * dtr; // In Radians - Width of transition region
+
+	densityLL = powf(minCellWidth/cellWidthLL, 4);
+	densityML = powf(minCellWidth/cellWidthML, 4);
+	densityHL = powf(minCellWidth/cellWidthHL, 4);
+
+	if ( fabs(lat) < latStepFunction) {
+		density = ((densityLL-densityML) * (1.0 + tanh( (lat_centLL - fabs(lat))/ widthLL)) / 2.0) + densityML;
+	} else {
+        density = ((densityML-densityHL) * (1.0 + tanh( (lat_centHL - fabs(lat))/ widthHL)) / 2.0) + densityHL;
+	}
+
+	return density;
 }/*}}}*/
 double pop_highres_density(const pnt &p){/*{{{*/
     double dtr;
